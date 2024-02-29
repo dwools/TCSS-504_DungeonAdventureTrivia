4de6b934e30ab42835329c37cf3c05f2805e71e8
Sanya <134664585+sanya0726@users.noreply.github.com>
2024-02-28 19:23:49 -0800

    Update save_game.py

--- save_game.py
+++ save_game.py
@@ -1,20 +1,24 @@
 import os
 import pickle
-
+from dungeon_adventure import DungeonAdventure
 
 class SaveGame:
 
     @staticmethod
     def pickle(game_data):
+        attributes = {}
+        attributes['player_position'] = game_data.player_position
         with open('dungeon_adventure.pickle', 'wb') as saved_file:
-            pickle.dump(game_data, saved_file)
+            pickle.dump(attributes, saved_file)
 
     @staticmethod
     def load_game():
         if os.path.exists("dungeon_adventure.pickle"):
             with open("dungeon_adventure.pickle", "rb") as f:
                 game_data = pickle.load(f)
-                return game_data  # returning the loaded game data
+                dungeon_adventure = DungeonAdventure()
+                dungeon_adventure.player_position = game_data['player_position']
+                return dungeon_adventure  # returning the loaded game data
 
 
 # creating an instance of SaveGame
